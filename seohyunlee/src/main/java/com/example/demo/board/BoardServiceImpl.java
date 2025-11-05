4297610f8e78cb9c4b8d1845e8b06e0c8f28b53c
Seohyun Lee <cwijiq3085@gmail.com>
2025-11-05 21:17:37 +0900

    seohyunlee day1

---
+++ seohyunlee/src/main/java/com/example/demo/board/BoardServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.demo.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+            @Autowired
+            private BoardDAO boardDAO;
+
+            public void insertBoard(BoardVO vo) {
+                boardDAO.insertBoard(vo);
+            }
+
+            public void updateBoard(BoardVO vo) {
+                boardDAO.updateBoard(vo);
+            }
+
+            public void deleteBoard(BoardVO vo) {
+                boardDAO.deleteBoard(vo);
+            }
+
+            public BoardVO getBoard(BoardVO vo) {
+                return boardDAO.getBoard(vo);
+            }
+
+            public java.util.List<BoardVO> getBoardList(BoardVO vo) {
+                return boardDAO.getBoardList(vo);
+            }
+        }
+
