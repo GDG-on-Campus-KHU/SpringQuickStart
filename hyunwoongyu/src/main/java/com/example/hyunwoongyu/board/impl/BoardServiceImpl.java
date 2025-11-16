afaebe570635e30e122dad88cc5f921d8bc23a14
유현웅 <yooho1030@naver.com>
2025-11-16 16:17:00 +0900

    DAY02 finished

--- hyunwoongyu/src/main/java/com/example/hyunwoongyu/board/impl/BoardServiceImpl.java
+++ hyunwoongyu/src/main/java/com/example/hyunwoongyu/board/impl/BoardServiceImpl.java
@@ -2,8 +2,8 @@ package com.example.hyunwoongyu.board.impl;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.hyunwoongyu.board.BoardService;
 import com.example.hyunwoongyu.board.BoardVO;
@@ -11,30 +11,30 @@ import com.example.hyunwoongyu.board.BoardVO;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
     @Autowired
-    private BoardDAO boardDAO;
+    private BoardDAOSpring boardDAOSpring;
 
     @Override
     public void insertBoard(BoardVO vo) {
-        boardDAO.insertBoard(vo);
+        boardDAOSpring.insertBoard(vo);
     }
 
     @Override
     public void updateBoard(BoardVO vo) {
-        boardDAO.updateBoard(vo);
+        boardDAOSpring.updateBoard(vo);
     }
 
     @Override
     public void deleteBoard(BoardVO vo) {
-        boardDAO.deleteBoard(vo);
+        boardDAOSpring.deleteBoard(vo);
     }
 
     @Override
     public BoardVO getBoard(BoardVO vo) {
-        return boardDAO.getBoard(vo);
+        return boardDAOSpring.getBoard(vo);
     }
 
     @Override
     public List<BoardVO> getBoardList(BoardVO vo) {
-        return boardDAO.getBoardList(vo);
+        return boardDAOSpring.getBoardList(vo);
     }
 }
